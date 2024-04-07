0f6c3610e98fd65124bf335ce63d45297b9d09d3
Vimal Kumar <vimalkumar02@hotmail.com>
2024-04-07 07:45:15 +0000

    Add infra#3

--- main.tf
+++ main.tf
@@ -23,14 +23,14 @@ provider "azurerm" {
 }
 
 resource "random_string" "uniquestring" {
-  length  = 20
-  special = false
-  upper   = false
+  length           = 20
+  special          = false
+  upper            = false
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "811-c596f270-provide-continuous-delivery-with-gith"
-  location = "eastus"
+  name     = "811-7d3141ad-provide-continuous-delivery-with-gith"
+  location = "southcentralus"
 }
 
 resource "azurerm_storage_account" "storageaccount" {
@@ -40,3 +40,4 @@ resource "azurerm_storage_account" "storageaccount" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
